47bede7368d67ce0e3a3e998b6e84b6d68281bce
SkyBroly <salemmouy@gmail.com>
2026-09-19 07:25:15 +0100

    configuration du fichier db.js

--- Backend/server.js
+++ Backend/server.js
@@ -41,7 +41,7 @@ app.use('/statistiques', statistiquesRoutes);
 
 app.use(errorHandler); // toujours en dernier, après toutes les routes
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Serveur démarré sur http://localhost:${PORT}`);
 });
